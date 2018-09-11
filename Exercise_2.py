99a9458a00065fd45635fbc9adcacb6498d00d72
Yulwei <gonzacalleja@gmail.com>
2018-09-11 19:27:42 +0200

    Another exercises

---
+++ Exercise_2.py
@@ -0,0 +1,8 @@
+
+print("Hello, Exercise 2")
+number_1 = int(input("Please enter the first number: "))
+number_2 = int(input("Please enter the second number: "))
+
+division = int(((number_1 - 1)//number_2)) + 1
+
+print("The rounded up division is: ", division)
---
+++ Exercise_3.py
@@ -0,0 +1,7 @@
+
+radius = input("Input the circles radius")
+PI = 3.1416
+
+area = PI*radius**2
+
+print(area)
